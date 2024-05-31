e18b3c9442febe4f6bf59352cd03bd0775385bf2
nickforbesy <144837288+nickforbesy@users.noreply.github.com>
2024-05-31 14:21:51 -0500

    Updated test_sampling.jl, added methods and plots for autocorrelation and rolling sample means

--- test_scripts/test_sampling.jl
+++ test_scripts/test_sampling.jl
@@ -2,6 +2,15 @@ using ergm
 using ergm.models
 using ergm.sampling
 
+using Pkg
+Pkg.add("StatsPlots")
+Pkg.add("Distributions")
+Pkg.add("StatsBase")
+
+using StatsPlots
+using Distributions
+using StatsBase
+
 half_n = 25
 n = 2 * half_n
 
@@ -13,6 +22,25 @@ end
 
 M = ScaffoldedEdgeTriangleModel(S, [0.0, -100.0])
 sampler = GibbsSampler(M, burn_in=n ^ 3, sample_interval=10 * n^2)
-graphs, stats = sample(sampler, 10, progress=true)
+graphs, stats = sample(sampler, 100, progress=true)
 
-stats .* [1 n]
+scaled_stats = stats .* [1 n]
+
+autocor_values = autocor(scaled_stats[:, 1])
+
+# Plot the autocorrelation function
+plot(autocor_values, seriestype=:stem, title="Autocorrelation of Gibbs Sampler Statistics", xlabel="Lag", ylabel="Autocorrelation")
+
+# Function to calculate rolling mean
+function rolling_mean(data, window_size)
+    n = length(data)
+    rolling_means = [mean(data[max(1, i-window_size+1):i]) for i in 1:n]
+    return rolling_means
+end
+
+# Compute rolling means for the first statistic (or any other of interest)
+window_size = 10  # Set window size for rolling mean
+rolling_means = rolling_mean(scaled_stats[:, 1], window_size)
+
+# Plot the rolling means
+plot(rolling_means, title="Rolling Mean of Gibbs Sampler Statistics", xlabel="Sample Index", ylabel="Rolling Mean", label="Window Size = $window_size")
