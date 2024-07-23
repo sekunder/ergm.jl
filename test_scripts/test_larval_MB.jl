279201ee7a27a898dae7a28b075394b78e50c562
Alex Kunin <alexkunin@creighton.edu>
2024-07-23 13:42:15 -0500

    working for n = 50! Not sure if set_parameters works.

--- test_scripts/test_larval_MB.jl
+++ test_scripts/test_larval_MB.jl
@@ -25,22 +25,22 @@ function log_message(message, verbose)
     end
 end
 
-scaffold = data.example_graph("larvalMB0.5")
+chi = "0.5"
+scaffold = data.example_graph("larvalMB" * chi)
 larvalMB = data.example_graph("larvalMB")
-n = 365
+n = 50
 
-small_scaffold = scaffold[1:50, 1:50]
-small_larvalMB = larvalMB[1:50, 1:50]
-small_n = 50
+small_scaffold = scaffold[1:n, 1:n]
+small_larvalMB = larvalMB[1:n, 1:n]
 
 model = ScaffoldedEdgeTriangleModel(small_scaffold, [0.0, 0.0])
 
-target_stats = [sum(small_larvalMB)/2, tr(small_scaffold^3)/small_n]
-real_density = [sum(small_larvalMB)/(small_n*(small_n-1)), tr((small_larvalMB)^3)/((small_n) * (small_n-1) *(small_n-2))]
-scaffold_density = [sum(small_scaffold)/(small_n*(small_n-1)), tr((small_scaffold)^3)/((small_n) * (small_n-1) *(small_n-2))]
+target_stats = [sum(small_larvalMB)/2, tr(small_scaffold^3)/n]
+real_density = [sum(small_larvalMB)/(n*(n-1)), tr((small_larvalMB)^3)/((n) * (n-1) *(n-2))]
+scaffold_density = [sum(small_scaffold)/(n*(n-1)), tr((small_scaffold)^3)/((n) * (n-1) *(n-2))]
 
-burn_in = 10 * small_n^2
-sample_interval = small_n^2
+burn_in = 10 * n^2
+sample_interval = n^2
 iterations = 50  
 learning_rate = 1.0 
 verbose = true  
@@ -65,8 +65,8 @@ new_graphs, new_stats = ergm.sampling.sample(new_sampler, 100, progress = true)
 As = [matrix_form(G) for G in new_graphs]
 Ss = [G.scaffold_edges for G in new_graphs]
 
-densities = cat([[sum(A) / (n * (n-1)) tr(A^3) / (n * (n-1) * (n-2))] for A in As]..., dims=1)
-scaff_only_densities = cat([[sum(M) / (n * (n-1)) tr(M^3) / (n * (n-1) * (n-2))] for M in Ss]..., dims=1)
+densities = cat([[sum(A) / (n * (n-1))  tr(A^3) / (n * (n-1) * (n-2))] for A in As]..., dims=1)
+scaff_only_densities = cat([[sum(M) / (n * (n-1))  tr(M^3) / (n * (n-1) * (n-2))] for M in Ss]..., dims=1)
 
 xs = 0.01:0.01:1
 er_curve = xs .^ 3
@@ -74,12 +74,15 @@ er_upper = sqrt.(xs) .^ 3
 xs_lower = 0.5:0.0001:1
 er_lower = xs_lower .* (2xs_lower .- 1)
 
-p = plot(xs, er_curve, color="gray", label="E-R", legend=:topleft, title="Densities with Recovered Parameters")
+p = plot(xs, er_curve, color="gray", label="E-R", legend=:topleft,
+         title="Densities with Recovered Parameters, chi=$chi\n$(thetas[end,:])")
 plot!(p, xs, er_upper, color="gray", linestyle=:dash, label=false)
 plot!(p, xs_lower[2:end], er_lower[2:end], color="gray", linestyle=:dash, label=false)
 scatter!(p, densities[:,1], densities[:,2], label="Samples")
 scatter!(p, scaff_only_densities[:,1], scaff_only_densities[:,2], marker=:x, label="Scaffold")
 scatter!(p, [real_density[1]], [real_density[2]], markershape = :star, markersize = 5, label="Original Graph Densities")
 scatter!(p, [scaffold_density[1]], [scaffold_density[2]], markershape = :star, markersize = 5, label="Original Scaffold Densities")
+xlabel!("Edge Density")
+ylabel!("Triangle Density")
 
 display(p)
